b7c775bbbfeb835ce1fab26a41ca9d33fb00ec15
Nerojust <nerojust4@gmail.com>
2018-04-11 14:11:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/mypizzaapp/SplashScreen.java
@@ -0,0 +1,51 @@
+package com.example.android.mypizzaapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //hides the title bar when the splash screen loads
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        //create an ImageView object called imageView
+        ImageView imageView = findViewById(R.id.imageView);
+
+        //creates and Animation object called animation
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fadesplashscreen);
+        imageView.startAnimation(animation);
+
+
+        Thread timer = new Thread(){
+
+            @Override
+            public void run() {
+
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                    super.run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        };
+
+        timer.start();
+    }
+}
